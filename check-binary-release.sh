652806651512d12255036dd55d851356bbf1285a
Till Westmann <tillw@apache.org>
2016-02-10 19:31:33 -0800

    extract LICENSE NOTICE DISCLAIMER from (nested) artifacts

--- check-binary-release.sh
+++ check-binary-release.sh
@@ -13,4 +13,28 @@ SHA1=ce3def891acff3d5766c62d95b68fe45b4a8a7b6
 
 REPO_URL=https://dist.apache.org/repos/dist/dev/incubator/asterixdb
 
+function nestedZips() {
+  ZIPFILE=$1
+  zipinfo -1 $ZIPFILE | grep \.zip
+}
+
+function unwrapZip() {
+  ARCHIVEZIP=$1
+  ARCHIVENAME=$(echo $ARCHIVEZIP | sed -e's/.zip$//')
+  mkdir $ARCHIVENAME
+  pushd $ARCHIVENAME
+  unzip ../$ARCHIVEZIP LICENSE NOTICE DISCLAIMER
+  for ZIP in $(nestedZips ../$ARCHIVEZIP)
+  do
+    unzip ../$ARCHIVEZIP $ZIP
+    pushd $(dirname $ZIP)
+    unwrapZip $(basename $ZIP)
+    popd
+  done
+  popd
+}
+
 checkArchives $ARCHIVENAME $MD5 $SHA1
+
+echo "--- Content ---"
+unwrapZip $ARCHIVENAME
