3be25cd3a2a9e1e4eb10205ab8e8311274dc2125
Till Westmann <tillw@apache.org>
2017-04-20 14:26:06 -0700

    apache-hyracks-0.3.1 RC2

--- check-source-release.sh
+++ check-source-release.sh
@@ -6,12 +6,12 @@ SCRIPTNAME=$(basename $0)
 . $(dirname $0)/check-release-lib.sh
 LOGFILE=$(pwd)/$SCRIPTNAME.log
 
-BASENAME=apache-asterixdb-0.9.1
+BASENAME=apache-hyracks-0.3.1
 ARCHIVENAME=$BASENAME-source-release
-SHA1=8fc212b478e1e3ef62865de233e509066dc3445d
-GERRIT_CHANGE=refs/changes/60/1660/1
+SHA1=e3d9ac77b8ca02b04fa3c7c64f74c8b2e4150e6f
+GERRIT_CHANGE=refs/changes/79/1679/1
 REPO=asterixdb
-REPO_DIR=asterixdb
+REPO_DIR=hyracks-fullstack
 
 REPO_URL=https://dist.apache.org/repos/dist/dev/asterixdb
 
@@ -22,66 +22,14 @@ function rat() {
     RATREPORT=$(pwd)/rat.report
     RATEXCLUDES=$(pwd)/rat.excludes
     cat > $RATEXCLUDES << EOF
-.*\.adm
-.*\.adm\.template
-.*\.ast
-.*\.cleaned
-.*\.csv
-.*\.dgen
-.*\.json
-.*\.plan
-.*\.tbl
-.*\.tbl\.big
-.*\.tbl\.verylong\.big
 .*\.txt
-.*\.regexadm
-.*\.regex
-.*\.ignore
-part-0000.*
-master
-slaves
-jobads\.new
-jobads\.old
-sample_08_header\.csv.*
-large_text
-tpch\.ddl
-overlapping\.data
-foo\.eps
-foo\.gpl
-gantt\.py
-vargantt1\.gpl
-vargantt1\.plt
-8\.dqgen
-classad-with-temporals\.classads
-customer\.scm
-lineitem\.scm
-nation\.scm
-orders\.scm
-part\.scm
-partsupp\.scm
-region\.scm
-supplier\.scm
-bootstrap-theme\.min\.css
-bootstrap\.min\.css
-glyphicons-halflings-regular\.svg
-angular\.min\.js
-bootstrap\.min\.js
-jquery-1\.12\.4\.min\.js
-jquery\.autosize-min\.js
-jquery\.min\.js
-rainbowvis\.js
-policy\.properties
-id_rsa
-id_rsa\.pub
-known_hosts
-LockRequestFile
-asm\.objectweb\.org_license\.html
-glassfish\.dev\.java\.net_public_CDDL_GPL_1_1\.html
-jline\.sourceforge\.net_license\.html
-www\.antlr\.org_license\.html
-www\.eclipse\.org_legal_epl-v10\.html
-www\.json\.org_license\.html
-www\.sun\.com_cddl_cddl\.html
+.*\.tbl
+.*tpch.ddl
+.*wordcount.tsv
+.*scanMicroSortWrite.out
+.*master
+.*slaves
+.*part-0
 EOF
     echo "running RAT with excludes in $RATEXCLUDES"
     java -jar ~/soft/apache-rat/apache-rat-0.12.jar -E $RATEXCLUDES -d $DIRNAME > $RATREPORT
@@ -122,7 +70,7 @@ check $BASENAME LICENSE
 
 echo "--- build ---"
 pushd $BASENAME
-mvn package $MVN_ARGS &> $LOGFILE
+mvn install $MVN_ARGS &> $LOGFILE
 popd
 tail $LOGFILE
 
