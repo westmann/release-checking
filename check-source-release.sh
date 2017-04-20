220d3c53641198598f91474dca4def402d050e6b
Till Westmann <tillw@apache.org>
2017-04-20 13:26:32 -0700

    apache-asterixdb-0.9.1

--- check-source-release.sh
+++ check-source-release.sh
@@ -6,30 +6,82 @@ SCRIPTNAME=$(basename $0)
 . $(dirname $0)/check-release-lib.sh
 LOGFILE=$(pwd)/$SCRIPTNAME.log
 
-BASENAME=apache-hyracks-0.3.1
+BASENAME=apache-asterixdb-0.9.1
 ARCHIVENAME=$BASENAME-source-release
-SHA1=9b004555bb45f8aec234cee1be04b264d603685a
+SHA1=8fc212b478e1e3ef62865de233e509066dc3445d
 GERRIT_CHANGE=refs/changes/60/1660/1
 REPO=asterixdb
-REPO_DIR=hyracks-fullstack
+REPO_DIR=asterixdb
 
 REPO_URL=https://dist.apache.org/repos/dist/dev/asterixdb
 
-#MVN_ARGS=-DskipTests
+MVN_ARGS=-DskipTests
 
 function rat() {
     DIRNAME=$1
     RATREPORT=$(pwd)/rat.report
     RATEXCLUDES=$(pwd)/rat.excludes
     cat > $RATEXCLUDES << EOF
-.*\.txt
+.*\.adm
+.*\.adm\.template
+.*\.ast
+.*\.cleaned
+.*\.csv
+.*\.dgen
+.*\.json
+.*\.plan
 .*\.tbl
-.*tpch.ddl
-.*wordcount.tsv
-.*scanMicroSortWrite.out
-.*master
-.*slaves
-.*part-0
+.*\.tbl\.big
+.*\.tbl\.verylong\.big
+.*\.txt
+.*\.regexadm
+.*\.regex
+.*\.ignore
+part-0000.*
+master
+slaves
+jobads\.new
+jobads\.old
+sample_08_header\.csv.*
+large_text
+tpch\.ddl
+overlapping\.data
+foo\.eps
+foo\.gpl
+gantt\.py
+vargantt1\.gpl
+vargantt1\.plt
+8\.dqgen
+classad-with-temporals\.classads
+customer\.scm
+lineitem\.scm
+nation\.scm
+orders\.scm
+part\.scm
+partsupp\.scm
+region\.scm
+supplier\.scm
+bootstrap-theme\.min\.css
+bootstrap\.min\.css
+glyphicons-halflings-regular\.svg
+angular\.min\.js
+bootstrap\.min\.js
+jquery-1\.12\.4\.min\.js
+jquery\.autosize-min\.js
+jquery\.min\.js
+rainbowvis\.js
+policy\.properties
+id_rsa
+id_rsa\.pub
+known_hosts
+LockRequestFile
+asm\.objectweb\.org_license\.html
+glassfish\.dev\.java\.net_public_CDDL_GPL_1_1\.html
+jline\.sourceforge\.net_license\.html
+www\.antlr\.org_license\.html
+www\.eclipse\.org_legal_epl-v10\.html
+www\.json\.org_license\.html
+www\.sun\.com_cddl_cddl\.html
 EOF
     echo "running RAT with excludes in $RATEXCLUDES"
     java -jar ~/soft/apache-rat/apache-rat-0.12.jar -E $RATEXCLUDES -d $DIRNAME > $RATREPORT
