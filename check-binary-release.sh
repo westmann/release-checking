98e52b335626441c56182abe2fe521fdeea83fa8
Till Westmann <tillw@apache.org>
2019-07-22 09:49:05 -0700

    asterix-server-0.9.5 - different package format

--- check-binary-release.sh
+++ check-binary-release.sh
@@ -6,29 +6,36 @@ SCRIPTNAME=$(basename $0)
 . $(dirname $0)/check-release-lib.sh
 LOGFILE=$(pwd)/$SCRIPTNAME.log
 
-BASENAME=apache-asterixdb-0.9.4
-ARCHIVENAME=$BASENAME
-SHA256=0b939231635f0c2328018f7064df9a4fa4b05b36835127a12eae4543141aecd9
+BASENAME=asterix-server-0.9.5
+RELEASENAME=apache-asterixdb-0.9.5
+ARCHIVENAME=$BASENAME-binary-assembly
+SHA256=d80ff63ea5796022f6ce58676d3954438ce703a1da06c5f382b8ace3d4719445
 
 REPO_URL=https://dist.apache.org/repos/dist/dev/asterixdb
 
 function unwrapZip() {
   local ARCHIVEZIP=$1
   local ARCHIVEDIR=$(echo $ARCHIVEZIP | sed -e's/.zip$//').exploded
-  echo "=== exploding $ARCHIVEZIP in $(pwd) into $ARCHIVEDIR"
   mkdir $ARCHIVEDIR
   pushd $ARCHIVEDIR >/dev/null
   unzip -q ../$ARCHIVEZIP
   popd >/dev/null
+  echo $ARCHIVEDIR
 }
 
 checkArchives $ARCHIVENAME $SHA256
 
 echo "--- Content ---"
-unwrapZip $ARCHIVENAME
 
-echo "=== check NOTICE file"
-echo "=== check LICENSE file against repo"
-echo "  > awk '/- repo/ { print $2 }' LICENSE | sort > lic.txt"
-echo "  > ls -1 repo/* | sort > repo.txt"
-echo "  > diff repo.txt lic.txt"
+ARCHIVEDIR=`unwrapZip $ARCHIVENAME`
+
+pushd $ARCHIVEDIR/$RELEASENAME
+
+echo "=== checking LICENSE against repo"
+awk '/- repo/ { print $2 }' LICENSE | sort > lic.txt
+ls -1 repo/* | sort > repo.txt
+diff repo.txt lic.txt
+
+echo "=== check NOTICE in $(PWD)/NOTICE"
+
+popd
